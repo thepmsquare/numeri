eff29f41d2530c0097fa2ea7af6fb910eb096216
Parth Mangtani <impmmmm@gmail.com>
2022-05-01 15:00:38 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,25 +12,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <meta name="description" content="Utility for conversion between roman and arabic numerals." />
+        <meta name="keywords" content="roman, arabic, numerals, conversion" />
+        <title>numeri</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#ffb300" />
       </Head>
       <Component {...pageProps} />
     </>
