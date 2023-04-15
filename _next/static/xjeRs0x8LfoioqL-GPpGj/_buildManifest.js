0630ee424cf8e2b115ad72c1199739988485dcfa
Parth Mangtani <impmmmm@gmail.com>
2023-04-15 16:06:28 +0530

    Deploy

---
+++ _next/static/xjeRs0x8LfoioqL-GPpGj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/72-c253aebb624dcc6d.js","static/css/35110a17f02a4ca0.css","static/chunks/pages/index-9b82d581a0c9a7b8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
